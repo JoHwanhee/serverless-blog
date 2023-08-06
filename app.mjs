fe0003930a443b35dc2c5ca13a8ba4ab3e9937f4
johwanhui <kikiki0611@gmail.com>
2023-08-07 03:25:01 +0900

    first commit

---
+++ app.mjs
@@ -0,0 +1,24 @@
+import express from 'express'
+import path from 'path'
+const __dirname = path.resolve();
+const app = express()
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/post/:id', (req, res) => {
+    const post = { title: '제목' + req.params.id, content: '내용' + req.params.id };
+    res.render('post', { post: post });
+});
+
+app.get('/', (req, res) => {
+    const posts = [
+        { title: '제목1', content: '내용1' },
+        { title: '제목2', content: '내용2' },
+    ];
+    console.log('aa')
+
+    res.render('index', { posts: posts });
+});
+
+export default app;
