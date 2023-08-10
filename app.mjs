b342549c2634879b614adc4c6bdbece0a218514f
johwanhui <kikiki0611@gmail.com>
2023-08-11 01:33:02 +0900

    refact: 리팩토링

--- app.mjs
+++ app.mjs
@@ -1,32 +1,30 @@
 import express from 'express'
 import path from 'path'
-const __dirname = path.resolve();
+
 const app = express()
+const __dirname = path.resolve()
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
+// App configuration
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/post/:id', (req, res) => {
-    const post = { title: '제목' + req.params.id, content: '내용' + req.params.id };
-    res.render('post', { post: post });
-});
-
-app.get('/', (req, res) => {
+// Route Handlers
+const renderHomepage = (req, res) => {
     const posts = [
         { title: '제목1', content: '내용1' },
         { title: '제목2', content: '내용2' },
-    ];
-    res.render('index', { posts: posts });
-});
+    ]
+    res.render('index', { posts })
+}
 
-app.get('/index.html', (req, res) => {
-    const posts = [
-        { title: '제목1', content: '내용1' },
-        { title: '제목2', content: '내용2' },
-    ];
-    res.render('index', { posts: posts });
-});
+const renderPost = (req, res) => {
+    const post = { title: '제목' + req.params.id, content: '내용' + req.params.id }
+    res.render('post', { post })
+}
 
+// Routes
+app.get(['/','/index.html'], renderHomepage) // Combine routes that render the same content
+app.get('/post/:id', renderPost)
 
-export default app;
+export default app
